ae5867d97221e4eda16023d3e0a0b87f4de5ff8b
RobbyPrograms <robertrolison99@gmail.com>
2022-11-25 15:14:16 -0700

    ttrying to fix

---
+++ main.py
@@ -0,0 +1,15 @@
+import random, time
+
+curr_cords = pag.position()
+afk_counter = 0                # counts how long we've been afk
+
+while True:
+    if pag.position() == curr_cords:
+        afk_counter += 1
+    else:
+        afk_counter = 0
+        curr_cords = pag.position()
+    if afk_counter > 5:
+        x = random.randint(2560, 5120)
+        y = random.randint(1, 1440)
+        
